39863921359b5c369c2961dca98159093a4bac4b
Jaroslav Havlin <380698+jhavlin@users.noreply.github.com>
2022-11-01 21:38:10 +0100

    Update candidate list

--- client/elm/src/Candidates.elm
+++ client/elm/src/Candidates.elm
@@ -22,84 +22,96 @@ all =
           , imgName = "andrej_babis"
           }
         , { id = 1
+          , name = "Jaroslav Bašta"
+          , firstName = "Jaroslav"
+          , surname = "Bašta"
+          , imgName = "jaroslav_basta"
+          }
+        , { id = 2
           , name = "Hynek Blaško"
           , firstName = "Hynek"
           , surname = "Blaško"
           , imgName = "hynek_blasko"
           }
-        , { id = 2
+        , { id = 3
           , name = "Tomáš Březina"
           , firstName = "Tomáš"
           , surname = "Březina"
           , imgName = "tomas_brezina"
           }
-        , { id = 3
+        , { id = 4
           , name = "Karel Diviš"
           , firstName = "Karel"
           , surname = "Diviš"
           , imgName = "karel_divis"
           }
-        , { id = 4
+        , { id = 5
           , name = "Pavel Fischer"
           , firstName = "Pavel"
           , surname = "Fischer"
           , imgName = "pavel_fischer"
           }
-        , { id = 5
+        , { id = 6
           , name = "Marek Hilšer"
           , firstName = "Marek"
           , surname = "Hilšer"
           , imgName = "marek_hilser"
           }
-        , { id = 6
+        , { id = 7
           , name = "Karel Janeček"
           , firstName = "Karel"
           , surname = "Janeček"
           , imgName = "karel_janecek"
           }
-        , { id = 7
+        , { id = 8
           , name = "Jiří Kotáb"
           , firstName = "Jiří"
           , surname = "Kotáb"
           , imgName = "jiri_kotab"
           }
-        , { id = 8
+        , { id = 9
           , name = "Ivo Mareš"
           , firstName = "Ivo"
           , surname = "Mareš"
           , imgName = "ivo_mares"
           }
-        , { id = 9
+        , { id = 10
+          , name = "Libor Michálek"
+          , firstName = "Libor"
+          , surname = "Michálek"
+          , imgName = "libor_michalek"
+          }
+        , { id = 11
           , name = "Danuše Nerudová"
           , firstName = "Danuše"
           , surname = "Nerudová"
           , imgName = "danuse_nerudova"
           }
-        , { id = 10
+        , { id = 12
           , name = "Petr Pavel"
           , firstName = "Petr"
           , surname = "Pavel"
           , imgName = "petr_pavel"
           }
-        , { id = 11
+        , { id = 13
           , name = "Josef Skála"
           , firstName = "Josef"
           , surname = "Skála"
           , imgName = "josef_skala"
           }
-        , { id = 12
+        , { id = 14
           , name = "Josef Středula"
           , firstName = "Josef"
           , surname = "Středula"
           , imgName = "josef_stredula"
           }
-        , { id = 13
+        , { id = 15
           , name = "Alena Vitásková"
           , firstName = "Alena"
           , surname = "Vitásková"
           , imgName = "alena_vitaskova"
           }
-        , { id = 14
+        , { id = 16
           , name = "Tomáš Zima"
           , firstName = "Tomáš"
           , surname = "Zima"
